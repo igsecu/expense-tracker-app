63c07fccb9bb3e68a5d8515bc00352d623538e02
Ignacio Cunial <igsecu@hotmail.com>
2024-04-22 15:45:13 -0300

    Added transactions from useContext

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,13 +1,27 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
+import TransactionItem from "./TransactionItem";
+
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
-    <div className="mt-3 border-bottom border-3">
+    <div
+      className="mt-3 border-bottom border-3 overflow-scroll"
+      style={{ minHeight: "250px", maxHeight: "250px" }}
+    >
       <h3>History</h3>
-      <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+      <ul className="list-group mb-3 rounded-0">
+        {transactions.length > 0 ? (
+          transactions.map((t) => (
+            <TransactionItem key={t.id} transaction={t} />
+          ))
+        ) : (
+          <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+            <p>No transactions to show!</p>
+          </div>
+        )}
       </ul>
     </div>
   );
