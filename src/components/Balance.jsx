1cfed3948075cf8cef86dad735a8f18e1465d7f4
Ignacio Cunial <igsecu@hotmail.com>
2024-04-22 15:44:51 -0300

    Added transactions from useContext

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,8 +1,17 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const total = transactions
+    .reduce((acc, item) => (acc += item.amount), 0)
+    .toFixed(2);
+
   return (
     <div className="d-flex flex-column justify-content-start">
       <h4 className="mb-0">YOUR BALANCE</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </div>
   );
 };
