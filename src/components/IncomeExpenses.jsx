24c4b1a73f224136bddcaf77f12308bb9f22f208
Ignacio Cunial <igsecu@hotmail.com>
2024-04-22 15:45:06 -0300

    Added transactions from useContext

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,13 +1,32 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
 const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const totalIncome = transactions
+    .filter((t) => t.amount > 0)
+    .reduce((acc, item) => {
+      return (acc += item.amount);
+    }, 0)
+    .toFixed(2);
+
+  const totalExpense = transactions
+    .filter((t) => t.amount < 0)
+    .reduce((acc, item) => {
+      return (acc += item.amount * -1);
+    }, 0)
+    .toFixed(2);
+
   return (
     <div className="d-flex justify-content-around mt-3 border-bottom border-3">
       <div className="me-3 text-center">
         <h4>INCOME</h4>
-        <h4 className="text-success">+$0.00</h4>
+        <h4 className="text-success">+${totalIncome}</h4>
       </div>
       <div className="text-center">
         <h4>EXPENSE</h4>
-        <h4 className="text-danger">-$0.00</h4>
+        <h4 className="text-danger">-${totalExpense}</h4>
       </div>
     </div>
   );
