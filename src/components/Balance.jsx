bca88be045cb16bbecef50fd38bd8269fa4a6e53
Ignacio Cunial <igsecu@hotmail.com>
2024-04-22 14:15:39 -0300

    Created Balance component

---
+++ src/components/Balance.jsx
@@ -0,0 +1,10 @@
+const Balance = () => {
+  return (
+    <div className="d-flex flex-column justify-content-start">
+      <h4 className="mb-0">YOUR BALANCE</h4>
+      <h1>$0.00</h1>
+    </div>
+  );
+};
+
+export default Balance;
