3c1e37f2d0ca1bee6808c3c9605ab51054df101f
Ignacio Cunial <igsecu@hotmail.com>
2024-04-22 14:30:14 -0300

    Created TransactionList component

---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,16 @@
+const TransactionList = () => {
+  return (
+    <div className="mt-3 border-bottom border-3">
+      <h3>History</h3>
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TransactionList;
